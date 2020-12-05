1588145671fff7362e88c181e9015af2c06395b0
speckledlemon <speckledlemon@protonmail.com>
2020-12-05 17:59:07 -0500

    Day 2 part 2 in Nim

--- 2020/day2.nim
+++ 2020/day2.nim
@@ -8,44 +8,56 @@ import unittest
 type
   Candidate = object
     password: string
-    reqCharacter: char
-    minReqCharacter: int
-    maxReqCharacter: int
+    ch: char
+    n1: int
+    n2: int
 
 func parseCandidate(s: string): Candidate =
   let
     tokens = s.split()
-    rangeReqCharacter = tokens[0].split('-').map(t => parseInt(t))
+    rangeCh = tokens[0].split('-').map(t => parseInt(t))
   Candidate(
     password: tokens[2],
-    reqCharacter: tokens[1][0],
-    minReqCharacter: rangeReqCharacter[0],
-    maxReqCharacter: rangeReqCharacter[1]
+    ch: tokens[1][0],
+    n1: rangeCh[0],
+    n2: rangeCh[1]
   )
 
-proc isValidPassword(candidate: Candidate): bool =
+func isValidPassword1(candidate: Candidate): bool =
   let
     frequencies = toCountTable(candidate.password)
     magic = -1
-    count = frequencies.getOrDefault(candidate.reqCharacter, magic)
+    count = frequencies.getOrDefault(candidate.ch, magic)
   if count == magic:
     return false
-  count in candidate.minReqCharacter..candidate.maxReqCharacter
+  count in candidate.n1..candidate.n2
+
+proc isValidPassword2(candidate: Candidate): bool =
+  let
+    creq = candidate.ch
+    has1 = candidate.password[candidate.n1 - 1] == creq
+    has2 = candidate.password[candidate.n2 - 1] == creq
+  has1 xor has2
 
 suite "day2":
-  test "passwordValidation":
-    let
-      example = """1-3 a: abcde
+  let
+    example = """1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc"""
-      exampleLines = example.splitlines()
-      candidates = exampleLines.map(s => parseCandidate(s))
-    check: candidates[0].isValidPassword
-    check: not candidates[1].isValidPassword
-    check: candidates[2].isValidPassword
+    exampleLines = example.splitlines()
+    candidates = exampleLines.map(s => parseCandidate(s))
+  test "passwordValidation1":
+    check: candidates[0].isValidPassword1
+    check: not candidates[1].isValidPassword1
+    check: candidates[2].isValidPassword1
+  test "passwordValidation2":
+    check: candidates[0].isValidPassword2
+    check: not candidates[1].isValidPassword2
+    check: not candidates[2].isValidPassword2
 
 when isMainModule:
   let
     inputLines = readAllLines("day2_input.txt")
     candidates = inputLines.map(l => parseCandidate(l))
-  echo "part1: ", candidates.map(c => isValidPassword(c)).count(true)
+  echo "part1: ", candidates.map(c => isValidPassword1(c)).count(true)
+  echo "part2: ", candidates.map(c => isValidPassword2(c)).count(true)
