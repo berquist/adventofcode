06a102c1f4ddc0cc30f27d1e3c134d02e1322cb0
speckledlemon <speckledlemon@protonmail.com>
2020-12-06 23:53:21 -0500

    Day 5 part 2 in Nim

--- 2020/day5.nim
+++ 2020/day5.nim
@@ -1,30 +1,49 @@
+import algorithm
 import math
 import sequtils
 import ./aoc_utils
 import unittest
 
-proc partition(binaryString: string, lowerChar, upperChar: char): int =
+func partition(binaryString: string, lowerChar, upperChar: char): int =
   var
     ilower = 0
     iupper = (2 ^ binaryString.len) - 1
-    ranges = newSeq[(int, int)]()
   for c in binaryString:
     if c == lowerChar:
       iupper = iupper - ((iupper - ilower) div 2) - 1
     else:
       ilower = ilower + ((iupper - ilower) div 2) + 1
-    ranges.add((ilower, iupper))
   assert ilower == iupper
   ilower
 
-proc getRow(pass: string): int =
+func getRow(pass: string): int =
   pass[..6].partition('F', 'B')
 
-proc getCol(pass: string): int =
+func getCol(pass: string): int =
   pass[7..9].partition('L', 'R')
 
-proc getSeatId(pass: string): int =
+func getSeatId(pass: string): int =
   pass.getRow * 8 + pass.getCol
+
+func groups[T](things: seq[T], count: int): seq[seq[T]] =
+  result = newSeq[seq[T]]()
+  var
+    start = 0
+    fin = start + count
+  while fin < things.len:
+    result.add(things[start..<fin])
+    start = fin
+    fin += count
+  if start < things.len:
+    result.add(things[start..<things.len])
+
+func findSeat(seatIds: seq[int]): int =
+  let sortedIds = seatIds.sorted
+  var prevId = sortedIds[0]
+  for currId in sortedIds[1..<sortedIds.len]:
+    if currId - prevId == 2:
+      return currId - 1
+    prevId = currId
 
 suite "day5":
   test "part 1":
@@ -44,7 +63,15 @@ suite "day5":
     check: ex4.getRow == 102
     check: ex4.getCol == 4
     check: ex4.getSeatId == 820
+  test "part 2":
+    check @["a", "b", "c", "d"].groups(2) == @[@["a", "b"], @["c", "d"]]
+    check @["a", "b", "c", "d", "e"].groups(2) == @[@["a", "b"], @["c", "d"], @["e"]]
+    check @["a", "b", "c", "d", "e", "f"].groups(3) == @[@["a", "b", "c"], @["d", "e", "f"]]
+    check @["a", "b", "c", "d", "e"].groups(3) == @[@["a", "b", "c"], @["d", "e"]]
 
 when isMainModule:
-  let input = "day5_input.txt".readAllLines
-  echo "part 1: ", input.mapIt(it.getSeatId).foldl(max(a, b))
+  let
+    input = "day5_input.txt".readAllLines
+    seatIds = input.mapIt(it.getSeatId)
+  echo "part 1: ", seatIds.foldl(max(a, b))
+  echo "part 2: ", seatIds.findSeat
