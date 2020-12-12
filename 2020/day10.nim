4fd4bcf0be490d1b561e8d518b9de3d93d9052e1
speckledlemon <speckledlemon@protonmail.com>
2020-12-11 22:45:21 -0500

    Day 10 part 2 in Nim [incomplete]

--- 2020/day10.nim
+++ 2020/day10.nim
@@ -6,17 +6,19 @@ import tables
 import unittest
 import ./aoc_utils
 
-# func makeCandidates(joltage: int, minDiff: int = 1, maxDiff: int = 3): seq[int] =
-#   for toDiff in minDiff..maxDiff:
-#     let candidate = joltage - toDiff
-#     if candidate >= 0:
-#       result.add candidate
+func makeCandidatesMinus(joltage: int, minDiff: int = 1, maxDiff: int = 3): seq[int] =
+  for toDiff in minDiff..maxDiff:
+    let candidate = joltage - toDiff
+    if candidate >= 0:
+      result.add candidate
 
 func makeCandidates(joltage: int, minDiff: int = 1, maxDiff: int = 3): seq[int] =
   for diff in minDiff..maxDiff:
     result.add joltage + diff
 
 func getDifferences(adapters: seq[int]): Table[int, int] =
+  result[1] = 0
+  result[3] = 0
   let
     # reversedAdapterJoltages = adapters.sorted(Descending)
     # allJoltages = @[reversedAdapterJoltages[0] + 3] & reversedAdapterJoltages & @[0]
@@ -33,23 +35,72 @@ func getDifferences(adapters: seq[int]): Table[int, int] =
   # allJoltages.incl 0
   # for joltage in allJoltages:
   #   candidates = joltage.makeCandidates
-  var
-    candidates: seq[int]
-    diff: int
+  var diff: int
   for joltage in orderedJoltages:
-    candidates = joltage.makeCandidates
-    for candidate in candidates:
+    for candidate in joltage.makeCandidates:
       if candidate in allJoltages:
-        diff = candidate - joltage
-        if diff notin result:
-          result[diff] = 1
-        else:
-          result[diff] += 1
+        result[candidate - joltage] += 1
         break
 
 func getResult(adapters: seq[int]): int =
   let diffs = adapters.getDifferences
   diffs[1] * diffs[3]
+
+# This is where I steal an algorithm from
+# https://github.com/networkx/networkx/blob/0ead17fb18334040b84dd614d1f12d6d3fa9e130/networkx/algorithms/simple_paths.py#L294
+# and stick it on top of a barebones graph object type, because patgraph
+# doesn't work for me.
+
+# type
+#   ## A stupid unweighted graph implementation based on adjacency lists.
+#   Graph = object
+#     impl: Table[int, seq[int]]
+
+# proc contains(g: Graph, item: int): bool =
+#   g.impl.contains(item)
+
+# proc `[]`(g: Graph, key: int): seq[int] =
+#   g.impl[key]
+
+# proc `[]=`(g: Graph, key: int, val: seq[int]) =
+#   g.impl[key] = val
+
+# proc addEdge(graph: var Graph, source, target: int) =
+#   if source notin graph:
+#     graph[source] = @[target]
+#   elif target notin graph[source]:
+#     graph[source].add(target)
+
+# proc addEdges(graph: var Graph, edges: openArray[int, int]) =
+#   for (source, target) in edges.items:
+#     graph.addEdge(source, target)
+
+# proc addEdges(edges: openArray[int, int]): Graph =
+#   result.addEdges edges
+
+# func allSimplePaths
+
+# proc toGraph[int](edges: seq[(int, int)]): Graph[int, float] =
+  # -- patgraph
+  # edges.graphFromEdges
+
+proc getUnorderedDifferences(adapters: seq[int]): seq[(int, int)] =
+  ## Generate all legal graph edges, then find all paths in the graph that
+  ## start and terminate at fixed points.
+  var
+    allJoltages = adapters.toHashSet
+    legalPairs: seq[(int, int)]
+  let
+    pathStart = 0
+    pathEnd = adapters.foldl(max(a, b)) + 3
+  allJoltages.incl pathStart
+  allJoltages.incl pathEnd
+  for joltage in allJoltages:
+    for candidate in joltage.makeCandidates:
+      if candidate in allJoltages:
+        legalPairs.add((joltage, candidate))
+  # echo legalPairs.toGraph
+  legalPairs
 
 suite "day10":
   let
@@ -61,7 +112,11 @@ suite "day10":
     check: adapters2.getDifferences == {1: 22, 3: 10}.toTable
     check: adapters1.getResult == 35
     check: adapters2.getResult == 220
+  # test "part 2":
+  #   echo adapters1.getUnorderedDifferences.sorted
+
 
 when isMainModule:
   let input = "day10_input.txt".readAllLines.mapIt(it.parseInt)
   echo "part 1: ", input.getResult
+  echo "part 2: "
