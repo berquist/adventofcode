c4805d1f499b3647303caaada3270f9b7e33be72
speckledlemon <speckledlemon@protonmail.com>
2021-12-21 23:58:21 -0500

    Day 1 part 2

--- 2021/day1.el
+++ 2021/day1.el
@@ -7,10 +7,11 @@
 (defconst example
   '(199 200 208 210 200 207 240 269 260 263))
 (defconst example-ans 7)
+(defconst example-ans-p2 5)
 
 (defun day1 (depths)
-  (let ((previous (car depths))
-        (count 0))
+  (let ((count 0)
+        (previous (car depths)))
     (dolist (current (cdr depths))
       (if (> current previous)
           (setq count (1+ count)))
@@ -20,4 +21,27 @@
 (ert-deftest test-day1 ()
   (should (equal (day1 example) example-ans)))
 
-(princ (day1 (mapcar #'string-to-number (s-lines (f-read "day1.txt")))))
+(defun day1-p2 (depths)
+  (let* ((count 0)
+         (first (car depths))
+         (second (cadr depths))
+         (third (caddr depths))
+         (previous-sum (+ first second third)))
+    (dolist (head (cdddr depths))
+      (let* ((cur-first second)
+             (cur-second third)
+             (cur-third head)
+             (cur-sum (+ cur-first cur-second cur-third)))
+        (if (> cur-sum previous-sum)
+            (setq count (1+ count)))
+        (setq first cur-first
+              second cur-second
+              third cur-third
+              previous-sum cur-sum)))
+    count))
+
+(ert-deftest test-day1-p2 ()
+  (should (equal (day1-p2 example) example-ans-p2)))
+
+(print (day1 (mapcar #'string-to-number (s-lines (f-read "day1.txt")))))
+(print (day1-p2 (mapcar #'string-to-number (s-lines (f-read "day1.txt")))))
