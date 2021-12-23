4103356f3bae840e6741217f7fa72004dc9f4b48
speckledlemon <speckledlemon@protonmail.com>
2021-12-22 20:37:45 -0500

    Day 2 part 2

--- 2021/day2.el
+++ 2021/day2.el
@@ -13,6 +13,7 @@
                     (down 8)
                     (forward 2)))
 (defconst example-ans 150)
+(defconst example-ans-p2 900)
 
 (defun day2 (moves)
   (let ((pos-v 0)
@@ -32,11 +33,32 @@
 (ert-deftest test-day2 ()
   (should (equal (day2 example) example-ans)))
 
+(defun day2-p2 (moves)
+  (let ((pos-v 0)
+        (pos-h 0)
+        (aim 0))
+    (dolist (move moves)
+      (let ((dir (car move))
+            (amt (cadr move)))
+        (cond
+         ((string= dir "forward")
+          (setq pos-h (+ pos-h amt)
+                pos-v (+ pos-v (* aim amt))))
+         ((string= dir "up")
+          (setq aim (- aim amt)))
+         ((string= dir "down")
+          (setq aim (+ aim amt))))))
+    (* pos-v pos-h)))
+
+(ert-deftest test-day2-p2 ()
+  (should (equal (day2-p2 example) example-ans-p2)))
+
 (defun transform-move (move-line)
   (let ((move (s-split " " move-line)))
-    `(,(car move) ,(string-to-number (cadr move)))))
+    (list (car move) (string-to-number (cadr move)))))
 
 (let* ((input (f-read "day2.txt"))
        (lines (seq-filter (lambda (line) (> (length line) 0)) (s-lines input)))
        (transformed-lines (mapcar #'transform-move lines)))
-  (print (day2 transformed-lines)))
+  (print (day2 transformed-lines))
+  (print (day2-p2 transformed-lines)))
